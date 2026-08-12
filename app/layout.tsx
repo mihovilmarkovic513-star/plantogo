0326deb8d83699ee4338f40e8f1cf615c63488ea
mihoiantun715 <mihoiantun715@gmail.com>
2026-08-12 16:05:37 +0200

    Fix AuthProvider placement in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AuthProvider>
-      <html lang="en">
-        <body>
-          {children}
-        </body>
-      </html>
-    </AuthProvider>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <AuthProvider>{children}</AuthProvider>
+      </body>
+    </html>
   );
 }
